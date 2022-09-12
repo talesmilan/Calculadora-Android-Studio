fb363c2ae13d6ebd0df51ec6bc879b444f25d417
talesmilan <100313945+talesmilan@users.noreply.github.com>
2022-09-12 11:49:59 -0300

    Funções matemáticas

--- Calculadora/app/src/main/java/projetos/calculadora/MainActivity.java
+++ Calculadora/app/src/main/java/projetos/calculadora/MainActivity.java
@@ -11,4 +11,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    //Função que soma dois números
+    public float somar(float a, float b) {
+        return a + b;
+    }
+
+    //Função que subtrai dois números
+    public float subtrair(float a, float b) {
+        return a - b;
+    }
+
+    //Função que divide dois números
+    public float dividir(float a, float b) {
+        return a / b;
+    }
+
+    //Função que multiplica dois números
+    public float multiplicar(float a, float b) {
+        return a * b;
+    }
+
 }
